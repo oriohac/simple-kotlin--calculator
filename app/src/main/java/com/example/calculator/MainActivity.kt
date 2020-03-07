a413ce420541b4143d40ba88e552349575d242ef
oriohac <orihac@gmail.com>
2020-03-07 23:08:39 +0100

    to be viewed only on portrait mode

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -15,59 +15,59 @@ class MainActivity : AppCompatActivity() {
         requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         setContentView(R.layout.activity_main)
 
-        one.setOnClickListener() {
+        one.setOnClickListener {
             evaluateExpression("1", clear = true)
         }
-        two.setOnClickListener() {
+        two.setOnClickListener {
             evaluateExpression("2", clear = true)
         }
-        three.setOnClickListener() {
+        three.setOnClickListener {
             evaluateExpression("3", clear = true)
         }
-        four.setOnClickListener() {
+        four.setOnClickListener {
             evaluateExpression("4", clear = true)
         }
-        five.setOnClickListener() {
+        five.setOnClickListener {
             evaluateExpression("5", clear = true)
         }
-        six.setOnClickListener() {
+        six.setOnClickListener {
             evaluateExpression("6", clear = true)
         }
-        seven.setOnClickListener() {
+        seven.setOnClickListener {
             evaluateExpression("7", clear = true)
         }
-        eight.setOnClickListener() {
+        eight.setOnClickListener {
             evaluateExpression("8", clear = true)
         }
-        nine.setOnClickListener() {
+        nine.setOnClickListener {
             evaluateExpression("9", clear = true)
         }
-        zero.setOnClickListener() {
+        zero.setOnClickListener {
             evaluateExpression("0", clear = true)
         }
-        dot.setOnClickListener() {
-            evaluateExpression(".", clear = true)
-        }
-        add.setOnClickListener() {
+        add.setOnClickListener {
             evaluateExpression("+", clear = true)
         }
-        minus.setOnClickListener() {
+        minus.setOnClickListener {
             evaluateExpression("-", clear = true)
         }
-        mult.setOnClickListener() {
+        mult.setOnClickListener {
             evaluateExpression("x", clear = true)
         }
-        div.setOnClickListener() {
+        div.setOnClickListener {
             evaluateExpression("/", clear = true)
         }
-        pow.setOnClickListener() {
+        dot.setOnClickListener {
+            evaluateExpression(".", clear = true)
+        }
+        pow.setOnClickListener {
             evaluateExpression("^", clear = true)
         }
-        clear.setOnClickListener() {
+        clear.setOnClickListener {
             textView.text = " "
             textView2.text = " "
         }
-        equal.setOnClickListener() {
+        equal.setOnClickListener {
             try {
                 val text = textView.text.toString()
                 val expression = ExpressionBuilder(text).build()
@@ -79,24 +79,23 @@ class MainActivity : AppCompatActivity() {
                     textView2.text = result.toString()
                 }
 
-            }catch (e: Exception){
-                Log.d("Exception","message:"+ e.message)
+            } catch (e: Exception) {
+                Log.d("Exception", "message:" + e.message)
             }
-        delete.setOnClickListener() {
+        }
+        delete.setOnClickListener {
             val text = textView.text.toString()
             if (text.isNotEmpty()){
                 textView.text = text.dropLast(1)
             }
             textView2.text = " "
         }
-        }
+
     }
     
 
     fun evaluateExpression(string: String, clear: Boolean) {
-        /*if(textView2.text.isNotEmpty()){
-            textView = ""
-    }*/
+
         if (clear) {
             textView2.text = " "
             textView.append(string)
